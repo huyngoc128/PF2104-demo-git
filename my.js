6ce95d64ce86d3242a78807ec005f933972b11ee
huyngoc128 <83991713+huyngoc128@users.noreply.github.com>
2021-05-18 20:19:07 +0700

    Update my.js

--- my.js
+++ my.js
@@ -2,4 +2,4 @@
 let r = +prompt("nhap ban kinh hinh tron:");
 const pi = 3.14;
 c = 2*r*3,14
-document.write("chu vi hinh tron co "+pi+" la:"+c+" cm");
+document.write("chu vi hinh tron co ban kinh r la:"+c+" cm");
